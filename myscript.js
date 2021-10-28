8d68b1ebafb2f99248b6d4c556b3f14fb3565535
iZestras <93310194+iZestras@users.noreply.github.com>
2021-10-28 08:54:55 +0200

    start of project

---
+++ myscript.js
@@ -0,0 +1,11 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+ctx.fillStyle = "#0000FF"
+ctx.fillRect(0, 0, 400, 250);
+
+ctx.fillStyle = "#FFFF00"
+ctx.fillRect(0, 100, 400, 45);
+
+ctx.fillStyle = "#FFFF00"
+ctx.fillRect(120, 0, 50, 250);
